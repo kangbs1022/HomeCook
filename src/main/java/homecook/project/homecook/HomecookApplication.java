09e982425f91dac4bff336ebab301f08cf018e07
peter <seungjunshose@gmail.com>
2025-11-17 14:22:35 +0900

    Initial commit

---
+++ src/main/java/homecook/project/homecook/HomecookApplication.java
@@ -0,0 +1,13 @@
+package homecook.project.homecook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/homecook/project/homecook/HomecookApplicationTests.java
@@ -0,0 +1,13 @@
+package homecook.project.homecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
